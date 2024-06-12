9132d1cc884e2ee474411531879546f4c2b2c89e
SandraKi <Sandra.Kindler@why.de>
2024-06-12 09:13:03 +0200

    feat: add mouse follow

--- components/TextMask.tsx
+++ components/TextMask.tsx
@@ -1,21 +1,55 @@
 'use client'
-import React from 'react';
-import { motion } from 'framer-motion';
+import React, { useEffect, useRef, useState } from 'react'
+import { motion, useTransform, useScroll } from "framer-motion"
+import { star, heart, hand, plane, lightning, note } from "./paths"
+import { interpolate } from "flubber"
+import { useFollowPointer } from './use-follow-pointer'
+
+const paths = [plane, lightning, hand, heart, note, star]
+const colors = [
+  "#000000",
+  "#0099ff",
+  "#8855ff",
+  "#ff0055",
+  "#ee4444",
+  "#ffcc00"
+]
+
+export const getIndex = (_: any, index: number) => index
 
 const TextMask = () => {
-  return (
-    <div className="relative overflow-hidden inline-block">
-      <motion.div
-        className="absolute top-0 left-0 size-full bg-black z-10"
-        initial={{ x: '-100%' }}
-        animate={{ x: '100%' }}
-        transition={{ duration: 2, repeat: Infinity, repeatType: 'reverse' }}
-      />
-      <div className="relative z-20">
-        <h1 className="text-5xl font-bold text-white mix-blend-difference">Hallo, Welt!</h1>
+    const [isAtEnd, setIsAtEnd] = useState(false)
+    const { scrollYProgress } = useScroll()
+    const moveX = useTransform(scrollYProgress, [0, 0.4], [0, 400])
+    const fill = useTransform(scrollYProgress, [0, 0.2, 0.4, 0.6, 0.8, 1], colors)
+    const path = useTransform(scrollYProgress, [0, 0.2, 0.4, 0.6, 0.8, 1], paths, {
+        mixer: (a, b) => interpolate(a, b, { maxSegmentLength: 0.1 })
+      })
+      const ref = useRef(null)
+      const { x, y } = useFollowPointer(ref)
+
+      useEffect(() => {
+          console.log(scrollYProgress.get())
+        setIsAtEnd(scrollYProgress.get() === 1)
+      }, [scrollYProgress])
+
+    return (
+    <motion.div className='h-[6000px] bg-white' style={{ x: moveX }}>
+        <motion.div ref={ref} className='w-fit h-screen sticky top-0' style={{x, y}} animate={{ rotate: isAtEnd ? 360 : 0, loop: Infinity }}>
+        <svg width="400" height="400" viewBox="0 0 400 400" className='h-full w-fit'>
+            <g transform="scale(17 17)">
+            <motion.path fill={fill} d={path} />
+            </g>
+        </svg>
+      </motion.div>
+      <div className='h-[2000px] absolute top-0 w-full'>
+      <h1 className='h-screen sticky top-0 font-bold text-[200px] text-white flex w-full justify-center pt-[100px] mix-blend-difference text-right'>Hello animated World!</h1>
       </div>
-    </div>
-  )
+      {/* <motion.div className="bg-black absolute left-0 bottom-0 h-[200px] rounded" animate={{ rotate: 360, loop: Infinity }}>
+      <h1 className='h-screen sticky top-0 font-bold text-[200px] text-white flex w-full justify-center pt-[100px] mix-blend-difference text-right'>Byyye!</h1>
+      </motion.div> */}
+      </motion.div>
+    )
 }
 
 export default TextMask
